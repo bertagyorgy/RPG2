8b94b18db68a1f966f0540c50fa7e8a89ebede6b
Berta György <111363208+bertagyorgy@users.noreply.github.com>
2025-04-10 13:26:26 +0200

    Add files via upload

---
+++ RPGjatek/RPGjatek/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace RPGjatek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstlbl = new System.Windows.Forms.Label();
+            this.secondlbl = new System.Windows.Forms.Label();
+            this.optionslbl = new System.Windows.Forms.Label();
+            this.option1lbl = new System.Windows.Forms.Label();
+            this.option2lbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // firstlbl
+            // 
+            this.firstlbl.AutoSize = true;
+            this.firstlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.firstlbl.ForeColor = System.Drawing.Color.White;
+            this.firstlbl.Location = new System.Drawing.Point(57, 382);
+            this.firstlbl.Name = "firstlbl";
+            this.firstlbl.Size = new System.Drawing.Size(431, 48);
+            this.firstlbl.TabIndex = 1;
+            this.firstlbl.Text = "You wake up and you find yourself in a Dark forest.\r\n\r\n";
+            // 
+            // secondlbl
+            // 
+            this.secondlbl.AutoSize = true;
+            this.secondlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.secondlbl.ForeColor = System.Drawing.Color.White;
+            this.secondlbl.Location = new System.Drawing.Point(25, 62);
+            this.secondlbl.Name = "secondlbl";
+            this.secondlbl.Size = new System.Drawing.Size(764, 48);
+            this.secondlbl.TabIndex = 3;
+            this.secondlbl.Text = "Suddenly you hear screaming from the north, you have to choose to go check it or " +
+    "leave it.\r\n\r\n";
+            // 
+            // optionslbl
+            // 
+            this.optionslbl.AutoSize = true;
+            this.optionslbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.optionslbl.ForeColor = System.Drawing.Color.White;
+            this.optionslbl.Location = new System.Drawing.Point(57, 478);
+            this.optionslbl.Name = "optionslbl";
+            this.optionslbl.Size = new System.Drawing.Size(55, 48);
+            this.optionslbl.TabIndex = 4;
+            this.optionslbl.Text = "Exits:\r\n\r\n";
+            // 
+            // option1lbl
+            // 
+            this.option1lbl.AutoSize = true;
+            this.option1lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.option1lbl.ForeColor = System.Drawing.Color.Lime;
+            this.option1lbl.Location = new System.Drawing.Point(85, 110);
+            this.option1lbl.Name = "option1lbl";
+            this.option1lbl.Size = new System.Drawing.Size(56, 24);
+            this.option1lbl.TabIndex = 5;
+            this.option1lbl.Text = "North";
+            // 
+            // option2lbl
+            // 
+            this.option2lbl.AutoSize = true;
+            this.option2lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.option2lbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.option2lbl.Location = new System.Drawing.Point(144, 110);
+            this.option2lbl.Name = "option2lbl";
+            this.option2lbl.Size = new System.Drawing.Size(52, 24);
+            this.option2lbl.TabIndex = 7;
+            this.option2lbl.Text = "West";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(62, 529);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(36, 527);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 24);
+            this.label8.TabIndex = 9;
+            this.label8.Text = ">";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(796, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Continue";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.option2lbl);
+            this.panel1.Controls.Add(this.option1lbl);
+            this.panel1.Controls.Add(this.secondlbl);
+            this.panel1.Location = new System.Drawing.Point(31, 366);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(919, 200);
+            this.panel1.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::RPGjatek.Properties.Resources.knight1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(720, 211);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 113);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox3.Image = global::RPGjatek.Properties.Resources.slime_animation;
+            this.pictureBox3.Location = new System.Drawing.Point(122, 222);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(101, 113);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.BackgroundImage = global::RPGjatek.Properties.Resources.forest;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(984, 335);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(984, 588);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.optionslbl);
+            this.Controls.Add(this.firstlbl);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "In the woods";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label firstlbl;
+        private System.Windows.Forms.Label secondlbl;
+        private System.Windows.Forms.Label optionslbl;
+        private System.Windows.Forms.Label option1lbl;
+        private System.Windows.Forms.Label option2lbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RPGjatek/RPGjatek/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RPGjatek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+        }
+        public int counter = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Parent = pictureBox2;
+            pictureBox3.Parent = pictureBox2;
+            if (File.Exists("scripts.txt"))
+            {
+                string[] beolvas = File.ReadAllLines("scripts.txt");
+                string elsosor = beolvas[counter];
+                string first = elsosor.Split(';')[0];
+                string second = elsosor.Split(';')[1];
+                string options = elsosor.Split(';')[2];
+                string option1 = elsosor.Split(';')[3];
+                string option2 = elsosor.Split(';')[4];
+
+                /*while (counter < beolvas.Length)
+                {*/
+                    firstlbl.Text = first;
+                    secondlbl.Text = second;
+                    optionslbl.Text = options;
+                    option1lbl.Text = option1;
+                    option2lbl.Text = option2;
+                /*}*/
+            }
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter++;
+
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
